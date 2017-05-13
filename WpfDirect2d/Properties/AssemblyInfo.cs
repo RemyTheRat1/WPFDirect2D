a6f1dbe56b6458db79ac0accef7e2bfca905c072
Lucas Christinson <lchristinson@gmail.com>
2017-05-12 22:06:29 -0700

    Updated nuget build process

--- WpfDirect2d/Properties/AssemblyInfo.cs
+++ WpfDirect2d/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WpfDirect2d")]
-[assembly: AssemblyDescription("WPF Direct2D Control")]
+[assembly: AssemblyDescription("WPF Direct2D Surface")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Lucas Christinson")]
 [assembly: AssemblyProduct("WpfDirect2d")]
@@ -34,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0-beta")]
